4b1fbd8a0c725f2a5e63fdc0788fc1a252faa661
Paul Truitt <paultruitt@Pauls-MacBook-Pro.local>
2023-01-31 14:21:50 -0500

    updating cors again

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(db,  {
 
 const app = express()
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'https://evening-scrubland-04264.herokuapp.com/' }))
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://127.0.0.1:5500' }))
 
 app.use(express.json())
 
